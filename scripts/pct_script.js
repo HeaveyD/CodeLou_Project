00b7ddc72ad9718eeba6f7423ec7c94f508cf010
David Heavey <dheavey13@gmail.com>
2017-06-07 00:15:11 -0400

    initial commit

---
+++ scripts/pct_script.js
@@ -0,0 +1,41 @@
+$("#pct_logo").hide().fadeIn(3000);
+
+
+//lightbox javascript (jquery)
+var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
+var $caption = $('<p></p>');
+
+//add image to overlay
+$overlay.append($image);
+
+//add caption to picture when clicked
+$overlay.append($caption);
+
+
+
+$('body').append($overlay);
+
+$('.imageGallery a').click(function(event){
+    event.preventDefault();
+   var imageLocation =  $(this).attr('href');
+
+//update overlay with image linked in
+$image.attr('src', imageLocation);
+
+  //show the overlay 
+  $overlay.show();
+
+//attach caption by selecting the child's alt attribute 
+var captionText = $(this).children('img').attr('alt');
+$caption.text(captionText);
+
+});
+
+$overlay.click(function(){
+    $overlay.hide();
+});
+
+
+
+
