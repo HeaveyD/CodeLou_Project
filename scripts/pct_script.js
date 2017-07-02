eb16e5a5564cb3744678da817b861cb64dc39e21
David Heavey <dheavey13@gmail.com>
2017-07-02 17:00:43 -0400

    Adjusted footer for larger screen sizes. Added interactivity to form with email validation.

--- scripts/pct_script.js
+++ scripts/pct_script.js
@@ -83,7 +83,30 @@ $('a[href*= "#"]')
 
  /*************************************This Script is for Form Submission***********/
 
- function formClear(){
+$('#email-field').keyup(function(){
+  if(this.value !== '' && !isValidEmailAddress(this.value)) {
+    $('#email-validation-message').show();
+    $('#form-submit').prop('disabled', true);
+  } else {
+    $('#email-validation-message').hide();
+    $('#form-submit').prop('disabled', false);
+  }
+});
+  
+$('#form-submit').click(function(e){
+  e.preventDefault();
+   document.getElementById("myForm").reset();
+    alert("Thank you for taking the time to join our email list! We'll review your information and provide you with an estimated finish date and tailored resources that may be useful to help you plan your adventure. Happy hiking and adventure on!");
+});
+
+ //Checks if email address is valid.  
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+
+  };
+
+  /*function formClear(){
     document.getElementById("myForm").reset();
     alert("Thank you for taking the time to join our email list! We'll review your information and provide you with an estimated finish date and tailored resources that may be useful to help you plan your adventure. Happy hiking and adventure on!");
- }
+ }*/
